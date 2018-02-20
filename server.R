4d342016949ed39b82d60ebc822b349d839d461d
KyuhyunJeong <kyuhyun.jeong@gmail.com>
2018-02-20 16:38:35 -0500

    initial format
    initial format consisted with the shiny dashboard.

---
+++ server.R
@@ -0,0 +1,41 @@
+## server.R ##
+
+shinyServer(
+  function(input, output) { 
+    
+    output$crisis_radius <- renderLeaflet({
+      crisis_address %>%
+        filter(is.na(lon) == F) %>%
+        filter(type == "Crisis Facility") %>%
+        leaflet() %>%
+        addProviderTiles(providers$Stamen.Toner) %>%
+        setView(
+          lng = -84.506836,
+          lat = 44.182205,
+          zoom = 7
+        ) %>%
+        addCircles(
+          lng = ~lon,
+          lat = ~lat,
+          color = "#FF9F55",
+          stroke = FALSE,
+          # Add radius in meters (= 60 miles)
+          radius = input$radius_len,
+          fillOpacity = 0.2
+        ) %>%
+        addCircleMarkers(
+          lng = ~lon,
+          lat = ~lat,
+          color = ~factpal(type),
+          popup = ~paste0(
+            "<b>Name:</b> ",htmlEscape(Name),"<br/>",
+            "<b>Address:</b> ",htmlEscape(Location),"<br/>",
+            "<b>Operated by:</b> ",htmlEscape(Operated),"<br/>",
+            "<b>Program Type:</b> ",htmlEscape(type)
+          ),
+          stroke = FALSE,
+          radius = 4,
+          fillOpacity = 0.6
+        )
+    })
+  })
